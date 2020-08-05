3d49a78d367c8b0b652cbb25edc5d93dbd7de0af
dw-sekikohei <seki.kohei@dandoli-works.com>
2020-08-05 14:34:08 +0900

    Updates

---
+++ precache-manifest.6b8a496f661a5037690bd8816e0a675c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f778c8a2b0af1bbf843a3b1bc6e0cb5",
+    "url": "/react-sample.git/index.html"
+  },
+  {
+    "revision": "688cd234db0edbe06c19",
+    "url": "/react-sample.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "69695ccf468145cf2315",
+    "url": "/react-sample.git/static/js/2.f7b4add9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sample.git/static/js/2.f7b4add9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "688cd234db0edbe06c19",
+    "url": "/react-sample.git/static/js/main.d6076e89.chunk.js"
+  },
+  {
+    "revision": "eeaa2813e23aa2526f9a",
+    "url": "/react-sample.git/static/js/runtime-main.e22ee030.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-sample.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
